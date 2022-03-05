5a93db8f7ba3b8f8be694a2c2f42788d3a5cb513
Michael Irimus <michaelirimus@gmail.com>
2022-03-05 16:22:21 +0100

    upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 const sass = require('sass');
 
 module.exports = {
